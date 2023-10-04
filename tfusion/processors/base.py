702c1ac4027426207dcac3940816d60aef34310e
hardesttype <hottab250@gmail.com>
2023-10-05 00:04:17 +0300

    Create base.py

---
+++ tfusion/processors/base.py
@@ -0,0 +1,13 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class DataProcessor(BaseEstimator, TransformerMixin):
+    """
+    Abstract class for data processors
+    """
+    def __init__(
+        self,
+        column_name: str,
+    ):
+        raise NotImplementedError('Abstract class!')
+  
