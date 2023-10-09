0a707b6c2f8389d9722ee2658afe130224efc0f6
hardesttype <hottab250@gmail.com>
2023-10-09 22:20:41 +0300

    Update mart.py

--- tfusion/compose/mart.py
+++ tfusion/compose/mart.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from typing import List, Dict
 
+from .source import DataSource
+
 
 class DataMart(BaseEstimator, TransformerMixin):
     def __init__(
